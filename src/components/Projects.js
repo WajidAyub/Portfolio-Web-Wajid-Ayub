3fd2dc6dc034cee44c88989e1dcc4cbfa7a7ffd6
Wajid Ayub <wajidayubwajid@gmail.com>
2026-06-27 00:13:55 +0500

    fix: Remove unused import causing Vercel build failure

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
-import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 
 const Projects = () => {
   const [ref, inView] = useInView({
